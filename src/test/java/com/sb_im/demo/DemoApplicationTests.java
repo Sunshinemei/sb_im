a50ca8685ae289f66ab35a58c2e022c177150d9b
akatsuke <1028572004@qq.com>
2019-12-17 16:17:49 +0800

    the first commit

---
+++ src/test/java/com/sb_im/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sb_im.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
